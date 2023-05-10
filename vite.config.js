101d1d4b7dbb546f8478f0bde387874cd186cff7
Xuan <Xuan@xu.com>
2023-05-10 15:43:25 +0800

    scss定制elementPlus主题v2

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// 自动按需导入
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
@@ -15,7 +16,10 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+        // 1. 配置elementPlus使用sass样式配置器
+        ElementPlusResolver({ importStyle: "sass" })
+      ],
     }),
     // 按需定制主题配置
     ElementPlus({
@@ -31,7 +35,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        // 自动导入定制化样式文件进行样式覆盖
+        // 2. 自动导入定制化样式文件进行样式覆盖
         additionalData: `
           @use "@/styles/element/index.scss" as *;
         `,
